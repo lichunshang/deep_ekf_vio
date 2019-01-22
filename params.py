771084d62672da4851ad0dfc93bdebc6981629c6
Chunshang Li <jack.chunshang.li@gmail.com>
2019-01-22 18:03:50 -0500

    got it running

--- params.py
+++ params.py
@@ -1,75 +1,114 @@
 import os
 
+
 class Parameters():
-	def __init__(self):
-		self.n_processors = 8
-		# Path
-		self.data_dir =  '/nfs/nas12.ethz.ch/fs1201/infk_ivc_students/cvg-students/chsiao/KITTI/'
-		self.image_dir = self.data_dir + '/images/'
-		self.pose_dir = self.data_dir + '/pose_GT/'
-		
-		self.train_video = ['00', '01', '02', '05', '08', '09']
-		self.valid_video = ['04', '06', '07', '10']
-		self.partition = None  # partition videos in 'train_video' to train / valid dataset  #0.8
-		
+    def __init__(self):
+        self.n_processors = 8
+        # Path
+        self.data_dir = '/home/cs4li/Dev/deep_ekf_vio/KITTI/'
+        self.image_dir = self.data_dir + 'images/'
+        self.pose_dir = self.data_dir + 'pose_GT/'
 
-		# Data Preprocessing
-		self.resize_mode = 'rescale'  # choice: 'crop' 'rescale' None
-		self.img_w = 608   # original size is about 1226
-		self.img_h = 184   # original size is about 370
-		self.img_means = (-0.14968217427134656, -0.12941663107068363, -0.1320610301921484)
-		self.img_stds = (1, 1, 1)  #(0.309122, 0.315710, 0.3226514)
-		self.minus_point_5 = True
+        self.train_video = ['00', '01', '02', '05', '08', '09']
+        self.valid_video = ['04', '06', '07', '10']
+        self.partition = None  # partition videos in 'train_video' to train / valid dataset  #0.8
 
-		self.seq_len = (5, 7)
-		self.sample_times = 3
+        # Data Preprocessing
+        self.resize_mode = 'rescale'  # choice: 'crop' 'rescale' None
+        self.img_w = 608  # original size is about 1226
+        self.img_h = 184  # original size is about 370
+        self.img_means = (-0.14968217427134656, -0.12941663107068363, -0.1320610301921484)
+        self.img_stds = (1, 1, 1)  # (0.309122, 0.315710, 0.3226514)
+        self.minus_point_5 = True
 
-		# Data info path
-		self.train_data_info_path = 'datainfo/train_df_t{}_v{}_p{}_seq{}x{}_sample{}.pickle'.format(''.join(self.train_video), ''.join(self.valid_video), self.partition, self.seq_len[0], self.seq_len[1], self.sample_times)
-		self.valid_data_info_path = 'datainfo/valid_df_t{}_v{}_p{}_seq{}x{}_sample{}.pickle'.format(''.join(self.train_video), ''.join(self.valid_video), self.partition, self.seq_len[0], self.seq_len[1], self.sample_times)
+        self.seq_len = (5, 7)
+        self.sample_times = 3
 
+        # Data info path
+        self.train_data_info_path = 'datainfo/train_df_t{}_v{}_p{}_seq{}x{}_sample{}.pickle'.format(
+            ''.join(self.train_video), ''.join(self.valid_video), self.partition, self.seq_len[0], self.seq_len[1],
+            self.sample_times)
+        self.valid_data_info_path = 'datainfo/valid_df_t{}_v{}_p{}_seq{}x{}_sample{}.pickle'.format(
+            ''.join(self.train_video), ''.join(self.valid_video), self.partition, self.seq_len[0], self.seq_len[1],
+            self.sample_times)
 
-		# Model
-		self.rnn_hidden_size = 1000
-		self.conv_dropout = (0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5)
-		self.rnn_dropout_out = 0.5
-		self.rnn_dropout_between = 0   # 0: no dropout
-		self.clip = None
-		self.batch_norm = True
-		# Training
-		self.epochs = 250
-		self.batch_size = 8
-		self.pin_mem = True
-		self.optim = {'opt': 'Adagrad', 'lr': 0.0005}
-					# Choice:
-					# {'opt': 'Adagrad', 'lr': 0.001}
-					# {'opt': 'Adam'}
-					# {'opt': 'Cosine', 'T': 100 , 'lr': 0.001}
-		
-		# Pretrain, Resume training
-		self.pretrained_flownet = None
-								# Choice:
-								# None
-								# './pretrained/flownets_bn_EPE2.459.pth.tar'  
-								# './pretrained/flownets_EPE1.951.pth.tar'
-		self.resume = True  # resume training
-		self.resume_t_or_v = '.train'
-		self.load_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
-		self.load_optimizer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer{}'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
+        # Model
+        self.rnn_hidden_size = 1000
+        self.conv_dropout = (0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5)
+        self.rnn_dropout_out = 0.5
+        self.rnn_dropout_between = 0  # 0: no dropout
+        self.clip = None
+        self.batch_norm = True
+        # Training
+        self.epochs = 250
+        self.batch_size = 8
+        self.pin_mem = True
+        self.optim = {'opt': 'Adagrad', 'lr': 0.0005}
+        # Choice:
+        # {'opt': 'Adagrad', 'lr': 0.001}
+        # {'opt': 'Adam'}
+        # {'opt': 'Cosine', 'T': 100 , 'lr': 0.001}
 
-		self.record_path = 'records/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.txt'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
-		self.save_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
-		self.save_optimzer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer'.format(''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0], self.seq_len[1], self.batch_size, self.rnn_hidden_size, '_'.join([k+str(v) for k, v in self.optim.items()]))
-		
-		
-		if not os.path.isdir(os.path.dirname(self.record_path)):
-			os.makedirs(os.path.dirname(self.record_path))
-		if not os.path.isdir(os.path.dirname(self.save_model_path)):
-			os.makedirs(os.path.dirname(self.save_model_path))
-		if not os.path.isdir(os.path.dirname(self.save_optimzer_path)):
-			os.makedirs(os.path.dirname(self.save_optimzer_path))
-		if not os.path.isdir(os.path.dirname(self.train_data_info_path)):
-			os.makedirs(os.path.dirname(self.train_data_info_path))
+        # Pretrain, Resume training
+        self.pretrained_flownet = './pretrained/flownets_bn_EPE2.459.pth.tar'
+        # Choice:
+        # None
+        # './pretrained/flownets_bn_EPE2.459.pth.tar'
+        # './pretrained/flownets_EPE1.951.pth.tar'
+        self.resume = False  # resume training
+        self.resume_t_or_v = '.train'
+        self.load_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model{}'.format(''.join(self.train_video),
+                                                                                           ''.join(self.valid_video),
+                                                                                           self.img_h, self.img_w,
+                                                                                           self.seq_len[0],
+                                                                                           self.seq_len[1],
+                                                                                           self.batch_size,
+                                                                                           self.rnn_hidden_size,
+                                                                                           '_'.join([k + str(v) for k, v
+                                                                                                     in
+                                                                                                     self.optim.items()]),
+                                                                                           self.resume_t_or_v)
+        self.load_optimizer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer{}'.format(
+            ''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0],
+            self.seq_len[1], self.batch_size, self.rnn_hidden_size,
+            '_'.join([k + str(v) for k, v in self.optim.items()]), self.resume_t_or_v)
+
+        self.record_path = 'records/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.txt'.format(''.join(self.train_video),
+                                                                                    ''.join(self.valid_video),
+                                                                                    self.img_h, self.img_w,
+                                                                                    self.seq_len[0], self.seq_len[1],
+                                                                                    self.batch_size,
+                                                                                    self.rnn_hidden_size, '_'.join(
+                    [k + str(v) for k, v in self.optim.items()]))
+        self.save_model_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.model'.format(''.join(self.train_video),
+                                                                                         ''.join(self.valid_video),
+                                                                                         self.img_h, self.img_w,
+                                                                                         self.seq_len[0],
+                                                                                         self.seq_len[1],
+                                                                                         self.batch_size,
+                                                                                         self.rnn_hidden_size, '_'.join(
+                    [k + str(v) for k, v in self.optim.items()]))
+        self.save_optimzer_path = 'models/t{}_v{}_im{}x{}_s{}x{}_b{}_rnn{}_{}.optimizer'.format(
+            ''.join(self.train_video), ''.join(self.valid_video), self.img_h, self.img_w, self.seq_len[0],
+            self.seq_len[1], self.batch_size, self.rnn_hidden_size,
+            '_'.join([k + str(v) for k, v in self.optim.items()]))
+
+        if not os.path.isdir(os.path.dirname(self.record_path)):
+            os.makedirs(os.path.dirname(self.record_path))
+        if not os.path.isdir(os.path.dirname(self.save_model_path)):
+            os.makedirs(os.path.dirname(self.save_model_path))
+        if not os.path.isdir(os.path.dirname(self.save_optimzer_path)):
+            os.makedirs(os.path.dirname(self.save_optimzer_path))
+        if not os.path.isdir(os.path.dirname(self.train_data_info_path)):
+            os.makedirs(os.path.dirname(self.train_data_info_path))
+
 
 par = Parameters()
+
+# elapsed time = 0.766002893447876
+# Numbers of frames in training dataset: 17437
+# mean_tensor =  [-0.151812640483464, -0.13357509111350818, -0.14181910364786987]
+# mean_np =  [88.78708011161852, 93.43778497818349, 91.33551888646076]
+# std_tensor =  [0.3174070577943728, 0.31982824445835345, 0.32372934976798146]
+# std_np =  [80.93941240862273, 81.557427180421, 82.55097977909139]
 
--- preprocess.py
+++ preprocess.py
@@ -9,112 +9,114 @@ from PIL import Image
 import torch
 import math
 
-def clean_unused_images():
-	seq_frame = {'00': ['000', '004540'],
-				'01': ['000', '001100'],
-				'02': ['000', '004660'],
-				'03': ['000', '000800'],
-				'04': ['000', '000270'],
-				'05': ['000', '002760'],
-				'06': ['000', '001100'],
-				'07': ['000', '001100'],
-				'08': ['001100', '005170'],
-				'09': ['000', '001590'],
-				'10': ['000', '001200']
-				}
-	for dir_id, img_ids in seq_frame.items():
-		dir_path = '{}{}/'.format(par.image_dir, dir_id)
-		if not os.path.exists(dir_path):
-			continue
 
-		print('Cleaning {} directory'.format(dir_id))
-		start, end = img_ids
-		start, end = int(start), int(end)
-		for idx in range(0, start):
-			img_name = '{:010d}.png'.format(idx)
-			img_path = '{}{}/{}'.format(par.image_dir, dir_id, img_name)
-			if os.path.isfile(img_path):
-				os.remove(img_path)
-		for idx in range(end+1, 10000):
-			img_name = '{:010d}.png'.format(idx)
-			img_path = '{}{}/{}'.format(par.image_dir, dir_id, img_name)
-			if os.path.isfile(img_path):
-				os.remove(img_path)
+def clean_unused_images():
+    seq_frame = {'00': ['000', '004540'],
+                 '01': ['000', '001100'],
+                 '02': ['000', '004660'],
+                 '03': ['000', '000800'],
+                 '04': ['000', '000270'],
+                 '05': ['000', '002760'],
+                 '06': ['000', '001100'],
+                 '07': ['000', '001100'],
+                 '08': ['001100', '005170'],
+                 '09': ['000', '001590'],
+                 '10': ['000', '001200']
+                 }
+    for dir_id, img_ids in seq_frame.items():
+        dir_path = '{}{}/'.format(par.image_dir, dir_id)
+        if not os.path.exists(dir_path):
+            continue
+
+        print('Cleaning {} directory'.format(dir_id))
+        start, end = img_ids
+        start, end = int(start), int(end)
+        for idx in range(0, start):
+            img_name = '{:010d}.png'.format(idx)
+            img_path = '{}{}/{}'.format(par.image_dir, dir_id, img_name)
+            if os.path.isfile(img_path):
+                os.remove(img_path)
+        for idx in range(end + 1, 10000):
+            img_name = '{:010d}.png'.format(idx)
+            img_path = '{}{}/{}'.format(par.image_dir, dir_id, img_name)
+            if os.path.isfile(img_path):
+                os.remove(img_path)
 
 
 # transform poseGT [R|t] to [theta_x, theta_y, theta_z, x, y, z]
 # save as .npy file
 def create_pose_data():
-	info = {'00': [0, 4540], '01': [0, 1100], '02': [0, 4660], '03': [0, 800], '04': [0, 270], '05': [0, 2760], '06': [0, 1100], '07': [0, 1100], '08': [1100, 5170], '09': [0, 1590], '10': [0, 1200]}
-	start_t = time.time()
-	for video in info.keys():
-		fn = '{}{}.txt'.format(par.pose_dir, video)
-		print('Transforming {}...'.format(fn))
-		with open(fn) as f:
-			lines = [line.split('\n')[0] for line in f.readlines()] 
-			poses = [ R_to_angle([float(value) for value in l.split(' ')]) for l in lines]  # list of pose (pose=list of 12 floats)
-			poses = np.array(poses)
-			base_fn = os.path.splitext(fn)[0]
-			np.save(base_fn+'.npy', poses)
-			print('Video {}: shape={}'.format(video, poses.shape))
-	print('elapsed time = {}'.format(time.time()-start_t))
+    info = {'00': [0, 4540], '01': [0, 1100], '02': [0, 4660], '03': [0, 800], '04': [0, 270], '05': [0, 2760],
+            '06': [0, 1100], '07': [0, 1100], '08': [1100, 5170], '09': [0, 1590], '10': [0, 1200]}
+    start_t = time.time()
+    for video in info.keys():
+        fn = '{}{}.txt'.format(par.pose_dir, video)
+        print('Transforming {}...'.format(fn))
+        with open(fn) as f:
+            lines = [line.split('\n')[0] for line in f.readlines()]
+            poses = [R_to_angle([float(value) for value in l.split(' ')]) for l in
+                     lines]  # list of pose (pose=list of 12 floats)
+            poses = np.array(poses)
+            base_fn = os.path.splitext(fn)[0]
+            np.save(base_fn + '.npy', poses)
+            print('Video {}: shape={}'.format(video, poses.shape))
+    print('elapsed time = {}'.format(time.time() - start_t))
 
 
 def calculate_rgb_mean_std(image_path_list, minus_point_5=False):
-	n_images = len(image_path_list)
-	cnt_pixels = 0
-	print('Numbers of frames in training dataset: {}'.format(n_images))
-	mean_np = [0, 0, 0]
-	mean_tensor = [0, 0, 0]
-	to_tensor = transforms.ToTensor()
+    n_images = len(image_path_list)
+    cnt_pixels = 0
+    print('Numbers of frames in training dataset: {}'.format(n_images))
+    mean_np = [0, 0, 0]
+    mean_tensor = [0, 0, 0]
+    to_tensor = transforms.ToTensor()
 
-	image_sequence = []
-	for idx, img_path in enumerate(image_path_list):
-		print('{} / {}'.format(idx, n_images), end='\r')
-		img_as_img = Image.open(img_path)
-		img_as_tensor = to_tensor(img_as_img)
-		if minus_point_5:
-			img_as_tensor = img_as_tensor - 0.5
-		img_as_np = np.array(img_as_img)
-		img_as_np = np.rollaxis(img_as_np, 2, 0)
-		cnt_pixels += img_as_np.shape[1]*img_as_np.shape[2]
-		for c in range(3):
-			mean_tensor[c] += float(torch.sum(img_as_tensor[c]))
-			mean_np[c] += float(np.sum(img_as_np[c]))
-	mean_tensor =  [v / cnt_pixels for v in mean_tensor]
-	mean_np = [v / cnt_pixels for v in mean_np]
-	print('mean_tensor = ', mean_tensor)
-	print('mean_np = ', mean_np)
+    image_sequence = []
+    for idx, img_path in enumerate(image_path_list):
+        print('{} / {}'.format(idx, n_images), end='\r')
+        img_as_img = Image.open(img_path)
+        img_as_tensor = to_tensor(img_as_img)
+        if minus_point_5:
+            img_as_tensor = img_as_tensor - 0.5
+        img_as_np = np.array(img_as_img)
+        img_as_np = np.rollaxis(img_as_np, 2, 0)
+        cnt_pixels += img_as_np.shape[1] * img_as_np.shape[2]
+        for c in range(3):
+            mean_tensor[c] += float(torch.sum(img_as_tensor[c]))
+            mean_np[c] += float(np.sum(img_as_np[c]))
+    mean_tensor = [v / cnt_pixels for v in mean_tensor]
+    mean_np = [v / cnt_pixels for v in mean_np]
+    print('mean_tensor = ', mean_tensor)
+    print('mean_np = ', mean_np)
 
-	std_tensor = [0, 0, 0]
-	std_np = [0, 0, 0]
-	for idx, img_path in enumerate(image_path_list):
-		print('{} / {}'.format(idx, n_images), end='\r')
-		img_as_img = Image.open(img_path)
-		img_as_tensor = to_tensor(img_as_img)
-		if minus_point_5:
-			img_as_tensor = img_as_tensor - 0.5
-		img_as_np = np.array(img_as_img)
-		img_as_np = np.rollaxis(img_as_np, 2, 0)
-		for c in range(3):
-			tmp = (img_as_tensor[c] - mean_tensor[c])**2
-			std_tensor[c] += float(torch.sum(tmp))
-			tmp = (img_as_np[c] - mean_np[c])**2
-			std_np[c] += float(np.sum(tmp))
-	std_tensor = [math.sqrt(v / cnt_pixels) for v in std_tensor]
-	std_np = [math.sqrt(v / cnt_pixels) for v in std_np]
-	print('std_tensor = ', std_tensor)
-	print('std_np = ', std_np)
+    std_tensor = [0, 0, 0]
+    std_np = [0, 0, 0]
+    for idx, img_path in enumerate(image_path_list):
+        print('{} / {}'.format(idx, n_images), end='\r')
+        img_as_img = Image.open(img_path)
+        img_as_tensor = to_tensor(img_as_img)
+        if minus_point_5:
+            img_as_tensor = img_as_tensor - 0.5
+        img_as_np = np.array(img_as_img)
+        img_as_np = np.rollaxis(img_as_np, 2, 0)
+        for c in range(3):
+            tmp = (img_as_tensor[c] - mean_tensor[c]) ** 2
+            std_tensor[c] += float(torch.sum(tmp))
+            tmp = (img_as_np[c] - mean_np[c]) ** 2
+            std_np[c] += float(np.sum(tmp))
+    std_tensor = [math.sqrt(v / cnt_pixels) for v in std_tensor]
+    std_np = [math.sqrt(v / cnt_pixels) for v in std_np]
+    print('std_tensor = ', std_tensor)
+    print('std_np = ', std_np)
 
 
 if __name__ == '__main__':
-	clean_unused_images()
-	create_pose_data()
-	
-	# Calculate RGB means of images in training videos
-	train_video = ['00', '02', '08', '09', '06', '04', '10']
-	image_path_list = []
-	for folder in train_video:
-		image_path_list += glob.glob('KITTI/images/{}/*.png'.format(folder))
-	calculate_rgb_mean_std(image_path_list, minus_point_5=True)
+    clean_unused_images()
+    create_pose_data()
 
+    # Calculate RGB means of images in training videos
+    train_video = ['00', '02', '08', '09', '06', '04', '10']
+    image_path_list = []
+    for folder in train_video:
+        image_path_list += glob.glob('KITTI/images/{}/*.png'.format(folder))
+    calculate_rgb_mean_std(image_path_list, minus_point_5=True)
