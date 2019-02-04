b3c2d5a4da3d85d304ee0c8afaea97895204e4fc
Chunshang Li <jack.chunshang.li@gmail.com>
2019-02-04 02:38:23 -0500

    finished stateful lstm, it is running

--- trainer.py
+++ trainer.py
@@ -16,15 +16,17 @@ class Trainer(object):
 
     def update_lstm_state(self, t_x_meta, lstm_states):
         _, seq_list, type_list, id_list = ImageSequenceDataset.decode_batch_meta_info(t_x_meta)
+        # lstm_states = (hidden state, cell state)
+
         assert (len(seq_list) == len(type_list) and
                 len(seq_list) == len(id_list) and
-                len(seq_list) == len(lstm_states[0]) and
-                len(seq_list) == len(lstm_states[1]))
+                len(seq_list) == lstm_states[0].size(1) and
+                len(seq_list) == lstm_states[1].size(1))
         num_batches = len(seq_list)
 
         for i in range(0, num_batches):
             key = "%s_%s_%d" % (seq_list[i], type_list[i], id_list[i][-1])
-            self.lstm_state_cache[key] = (lstm_states[0][i], lstm_states[1][i],)
+            self.lstm_state_cache[key] = (lstm_states[0][:, i, :], lstm_states[1][:, i, :],)
 
     def retrieve_lstm_state(self, t_x_meta):
         _, seq_list, type_list, id_list = ImageSequenceDataset.decode_batch_meta_info(t_x_meta)
@@ -35,8 +37,8 @@ class Trainer(object):
         lstm_cell_states = []
 
         for i in range(0, num_batches):
-            key = "%s_%s_%d" % (seq_list[i], type_list[i], id_list[i][-1])
-            if key in self.lstm_state_cache[key]:
+            key = "%s_%s_%d" % (seq_list[i], type_list[i], id_list[i][0])
+            if key in self.lstm_state_cache:
                 tmp = self.lstm_state_cache[key]
             else:
                 num_layers = self.model.rnn.num_layers
@@ -45,16 +47,21 @@ class Trainer(object):
             lstm_hidden_states.append(tmp[0])
             lstm_cell_states.append(tmp[1])
 
-        return torch.stack(lstm_hidden_states), torch.stack(lstm_cell_states)
+        return [torch.stack(lstm_hidden_states, dim=1), torch.stack(lstm_cell_states, dim=1), ]
 
     def get_loss(self, t_x_meta, x, y):
         prev_lstm_states = None
         if par.stateful_training:
             prev_lstm_states = self.retrieve_lstm_state(t_x_meta)
+            prev_lstm_states[0] = prev_lstm_states[0].cuda()
+            prev_lstm_states[1] = prev_lstm_states[1].cuda()
 
         predicted, lstm_states = self.model.forward(x, prev_lstm_states)
 
         if par.stateful_training:
+            lstm_states = list(lstm_states)
+            lstm_states[0] = lstm_states[0].detach().cpu()
+            lstm_states[1] = lstm_states[1].detach().cpu()
             self.update_lstm_state(t_x_meta, lstm_states)
 
         # Weighted MSE Loss
