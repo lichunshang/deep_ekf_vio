fixed log3 numerical problem

data_helper.py
@@ -149,9 +149,9 @@ class SubseqDataset(Dataset):
         for i in range(1, len(subseq.gt_poses)):
             T_i_vkm1 = subseq.gt_poses[i - 1]
             T_i_vk = subseq.gt_poses[i]
-            T_vkm1_k = se3_math.reorthogonalize_SE3(np.linalg.inv(T_i_vkm1).dot(T_i_vk))
-            r_vk_vkm1_vkm1 = T_vkm1_k[0:3, 3]  # get the translation from T
-            phi_vkm1_vk = se3_math.log_SO3(T_vkm1_k[0:3, 0:3])
+            T_vkm1_vk = se3_math.reorthogonalize_SE3(np.linalg.inv(T_i_vkm1).dot(T_i_vk))
+            r_vk_vkm1_vkm1 = T_vkm1_vk[0:3, 3]  # get the translation from T
+            phi_vkm1_vk = se3_math.log_SO3(T_vkm1_vk[0:3, 0:3])
             gt_rel_poses.append(np.concatenate([r_vk_vkm1_vkm1, phi_vkm1_vk, ]))
 
         gt_rel_poses = torch.FloatTensor(gt_rel_poses)

se3_math.py
@@ -1,6 +1,7 @@
 import numpy as np
 from log import logger
 import transformations
+import scipy.linalg
 
 
 def skew3(v):
@@ -39,18 +40,23 @@ def log_SO3(C):
     else:
         phi = np.arccos((np.trace(C) - 1) / 2)
 
-    if abs(phi) > 1e-12:
+    assert (phi >= 0 and np.sin(phi) >= 0)
+
+    # theta = unskew3(scipy.linalg.logm(C))
+
+    if np.sin(phi) > 1e-2:
         u = unskew3(C - np.transpose(C)) / (2 * np.sin(phi))
         theta = phi * u
     else:
-        theta = 0.5 * unskew3(C - C.transpose())
+        # theta = 0.5 * unskew3(C - C.transpose())
+        theta = unskew3(scipy.linalg.logm(C))
 
     return theta
 
 
 def exp_SO3(phi):
     phi_norm = np.linalg.norm(phi)
-    if np.abs(phi_norm) > 1e-12:
+    if np.abs(phi_norm) > 1e-8:
         unit_phi = phi / phi_norm
         unit_phi_skewed = skew3(unit_phi)
         m = np.eye(3, 3) + np.sin(phi_norm) * unit_phi_skewed + \